ac449b53dd808e5d20ada3e39cfe00b8a874beb1
Rupalii03 <138228651+Rupalii03@users.noreply.github.com>
2024-02-25 08:56:41 +0000

    tutorial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId="432556"
+let accountEmail="roop@gmail.com"
+var accountPassword="6543"
+accountCity="jaipur"
+
+accountEmail="tyu@gmail.com"
+
+accountPassword="7800"
+accountCity="gzb"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword])
